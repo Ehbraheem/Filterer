651af63cd7d770f16ba5f6d9a66d3d49f0a58004
Ehbraheem <ehbraheem@gmail.com>
2017-04-03 14:39:37 +0100

    Cleanup and add the image

--- RGBAImage.swift
+++ RGBAImage.swift
@@ -1,92 +1,149 @@
 import UIKit
 
 public struct Pixel {
+    
     public var value: UInt32
     
     public var red: UInt8 {
+        
         get {
+            
             return UInt8(value & 0xFF)
+            
         }
+        
         set {
+            
             value = UInt32(newValue) | (value & 0xFFFFFF00)
+            
         }
+        
     }
     
     public var green: UInt8 {
+        
         get {
+            
             return UInt8((value >> 8) & 0xFF)
+            
         }
+        
         set {
+            
             value = (UInt32(newValue) << 8) | (value & 0xFFFF00FF)
+            
         }
+        
     }
     
     public var blue: UInt8 {
+        
         get {
+            
             return UInt8((value >> 16) & 0xFF)
+            
         }
+        
         set {
+            
             value = (UInt32(newValue) << 16) | (value & 0xFF00FFFF)
+            
         }
+        
     }
     
     public var alpha: UInt8 {
+        
         get {
+            
             return UInt8((value >> 24) & 0xFF)
+            
         }
+        
         set {
+            
             value = (UInt32(newValue) << 24) | (value & 0x00FFFFFF)
+            
         }
+        
     }
+    
 }
 
 public struct RGBAImage {
+    
     public var pixels: [Pixel]
     
     public var width: Int
+    
     public var height: Int
     
     public init?(image: UIImage) {
-        guard let cgImage = image.CGImage else { return nil }
         
-        // Redraw image for correct pixel format
+        guard let cgImage = image.cgImage else { return nil }
+        
+        // Redraw image for correct pixel format/Users/chinmaydb/Documents/Codes/IOS Data/IOS Projects/Filterer/Filterer/RGBAImage.swift:66:5: 'CGContextDrawImage' is unavailable: Use draw(_:in:)
+        
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         
-        var bitmapInfo: UInt32 = CGBitmapInfo.ByteOrder32Big.rawValue
-        bitmapInfo |= CGImageAlphaInfo.PremultipliedLast.rawValue & CGBitmapInfo.AlphaInfoMask.rawValue
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
+        
+        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
         
         width = Int(image.size.width)
+        
         height = Int(image.size.height)
+        
         let bytesPerRow = width * 4
         
-        let imageData = UnsafeMutablePointer<Pixel>.alloc(width * height)
+        let imageData = UnsafeMutablePointer<Pixel>.allocate(capacity: width * height)
         
-        guard let imageContext = CGBitmapContextCreate(imageData, width, height, 8, bytesPerRow, colorSpace, bitmapInfo) else { return nil }
-        CGContextDrawImage(imageContext, CGRect(origin: CGPointZero, size: image.size), cgImage)
+        guard let imageContext = CGContext(data: imageData, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else { return nil }
+        
+        // CGContextDrawImage(imageContext, CGRect(origin: CGPointZero, size: image.size), cgImage)
+        
+        // CGContextDrawImage(imageContext, CGRect(origin: CGPoint(x: 0,y :0), size: image.size), cgImage)
+        
+        // imageContext.draw(image.cgImage!, in: CGRect(x: 0.0,y: 0.0,width: image.size.width,height: image.size.height))
+        
+        imageContext.draw(cgImage, in: CGRect(x: 0.0,y: 0.0,width: image.size.width,height: image.size.height))
         
         let bufferPointer = UnsafeMutableBufferPointer<Pixel>(start: imageData, count: width * height)
+        
         pixels = Array(bufferPointer)
         
-        imageData.destroy()
-        imageData.dealloc(width * height)
+        imageData.deinitialize()
+        
+        imageData.deallocate(capacity: width * height)
+        
     }
     
     public func toUIImage() -> UIImage? {
+        
         let colorSpace = CGColorSpaceCreateDeviceRGB()
-        var bitmapInfo: UInt32 = CGBitmapInfo.ByteOrder32Big.rawValue
-        bitmapInfo |= CGImageAlphaInfo.PremultipliedLast.rawValue & CGBitmapInfo.AlphaInfoMask.rawValue
+        
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
+        
+        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
         
         let bytesPerRow = width * 4
         
-        let imageDataReference = UnsafeMutablePointer<Pixel>(pixels)
-        defer {
-            imageDataReference.destroy()
-        }
-        let imageContext = CGBitmapContextCreateWithData(imageDataReference, width, height, 8, bytesPerRow, colorSpace, bitmapInfo, nil, nil)
+        let imageDataReference = UnsafeMutablePointer<Pixel>(mutating: pixels)
         
-        guard let cgImage = CGBitmapContextCreateImage(imageContext) else {return nil}
-        let image = UIImage(CGImage: cgImage)
+        defer {
+            
+            imageDataReference.deinitialize()
+            
+        }
+        
+        let imageContext = CGContext(data: imageDataReference, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil)
+        
+        guard let cgImage = imageContext!.makeImage() else {return nil}
+        
+        let image = UIImage(cgImage: cgImage)
         
         return image
+        
     }
+    
 }
