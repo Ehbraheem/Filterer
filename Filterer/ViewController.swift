8d0e30be971e918c960c54b57a76338a02169996
Ehbraheem <ehbraheem@gmail.com>
2017-04-07 19:45:07 +0100

    Added a new view

--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -11,54 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var filteredImage: UIImage?
-    var image: UIImage?
     
     @IBOutlet weak var imageView: UIImageView!
+   
+    @IBOutlet var secondaryMenu: UIView!
     
-    @IBOutlet weak var imageToggle: UIButton!
+    @IBOutlet weak var bottomMenu: UIView!
     
+    @IBOutlet weak var filterButton: UIButton!
     
-    @IBAction func onImageToggle(_ sender: UIButton) {
-        if (imageToggle.isSelected) {
-            imageView.image = image
-            imageToggle.isSelected = false
-        } else {
-            imageView.image = filteredImage
-            imageToggle.isSelected = true
-        }
-        
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        image = #imageLiteral(resourceName: "scenery")
-        
-        var rgbaImage = RGBAImage(image: image!)!
-
-        let avgRed = 107
-        
-        for y in 0..<rgbaImage.height {
-            for x in 0..<rgbaImage.width {
-                let index = y * rgbaImage.width + x
-                
-                var pixel = rgbaImage.pixels[index]
-                
-                let redDelta = Int(pixel.red) - avgRed
-                
-                var modifier = 1 + 4 * (Double(y) / Double (rgbaImage.height))
-                if (Int(pixel.red) < avgRed) {
-                    modifier = 1
-                }
-                
-                pixel.red = UInt8(max(min(255, Int(round(Double(avgRed) + modifier * Double(redDelta)))), 0))
-                rgbaImage.pixels[index] = pixel
-            }
-        }
-        
-        filteredImage = rgbaImage.toUIImage()
-        
-        
+        secondaryMenu.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -67,5 +33,35 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onFilter(_ sender: UIButton) {
+        if (sender.isSelected) {
+            hideSecondaryView()
+            sender.isSelected = false
+        } else {
+            showSecondaryMenu()
+            sender.isSelected = true
+        }
+        
+    }
+    
+    func showSecondaryMenu() {
+        view.addSubview(secondaryMenu)
+        
+        let bottomContraint = secondaryMenu.bottomAnchor.constraint(equalTo: bottomMenu.topAnchor)
+        
+        let leftContraint = secondaryMenu.leftAnchor.constraint(equalTo: view.leftAnchor)
+        
+        let rightContraint = secondaryMenu.rightAnchor.constraint(equalTo: view.rightAnchor)
+        
+        let heightContraint = secondaryMenu.heightAnchor.constraint(equalToConstant: 44)
+        
+        NSLayoutConstraint.activate([bottomContraint, leftContraint, rightContraint, heightContraint])
+        
+        view.layoutIfNeeded()
+    }
+    
+    func hideSecondaryView() {
+        secondaryMenu.removeFromSuperview()
+    }
 }
 
